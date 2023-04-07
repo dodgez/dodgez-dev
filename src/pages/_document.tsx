507f644c8ad44790563d3af70300128b08dcbc3f
dodgez <zacharysdodge@gmail.com>
2023-04-07 09:59:19 -0700

    feat: initial refactor with Monaco editor

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Zach Dodge's personal website" />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
