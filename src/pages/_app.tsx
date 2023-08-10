290775d95126a1dcd927e4aa068c9da88afa951c
dodgez <zacharysdodge@gmail.com>
2023-08-10 12:12:51 -0700

    feat: add about info on /

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,9 +10,10 @@ import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import NextLink from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const darkTheme = createTheme({
   palette: {
@@ -20,41 +21,58 @@ const darkTheme = createTheme({
   },
 });
 
-const pages = [{ href: 'https://git.dodgez.dev', label: 'Git' }];
+const pages = [{ href: '/playground', label: 'Code Playground' }];
 
 export default function App({ Component, pageProps }: AppProps) {
+  const pathname = usePathname();
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <CssBaseline />
-      <Box display="flex" flexDirection="column" height="100vh">
+      <Box
+        display="flex"
+        flexDirection="column"
+        height={pathname === '/playground' ? '100vh' : '100%'}
+      >
         <AppBar position="relative">
           <Toolbar>
-            <Typography
-              color={darkTheme.palette.primary.dark}
-              component="div"
-              variant="h6"
-            >
-              dodgez.dev
-            </Typography>
+            <NextLink href="/" legacyBehavior passHref>
+              <Link
+                sx={{
+                  textDecoration: 'none',
+                }}
+              >
+                dodgez.dev
+              </Link>
+            </NextLink>
             <Box display="flex" flexGrow={1} ml={4}>
               <Stack direction="row" spacing={2}>
                 {pages.map((page) => (
-                  <Link
+                  <NextLink
                     href={page.href}
                     key={page.href}
-                    sx={{
-                      color: 'white',
-                      paddingTop: '4px',
-                      textDecoration: 'none',
-                    }}
+                    legacyBehavior
+                    passHref
                   >
-                    {page.label}
-                  </Link>
+                    <Link
+                      sx={{
+                        color: 'white',
+                        textDecoration: 'none',
+                      }}
+                    >
+                      {page.label}
+                    </Link>
+                  </NextLink>
                 ))}
               </Stack>
+            </Box>
+            <Box>
+              <Link href="https://github.com/dodgez/dodgez-dev" target="_blank">
+                Source
+              </Link>
             </Box>
           </Toolbar>
         </AppBar>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,128 @@
-import Editor, { useMonaco } from '@monaco-editor/react';
+import AboutBlurb from '@/components/AboutBlurb';
+import Education from '@/components/Education';
+import Experience from '@/components/Experience';
+import ProfileBlurb from '@/components/ProfileBlurb';
+import Tech from '@/components/Tech';
 import Box from '@mui/material/Box';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Stack from '@mui/material/Stack';
+import Switch from '@mui/material/Switch';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import Head from 'next/head';
-import { useEffect } from 'react';
+import {
+  ChangeEvent,
+  SyntheticEvent,
+  useCallback,
+  useMemo,
+  useState,
+} from 'react';
 
-const DEFAULT_CODE_URL =
-  'https://raw.githubusercontent.com/dodgez/dodgez-dev/main/src/pages/index.tsx';
+export default function Home() {
+  const [isTabbedView, setTabbedView] = useState(false);
+  const handleViewChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) =>
+      setTabbedView(event.target.checked),
+    [],
+  );
 
-export async function getStaticProps() {
-  const defaultCode = await fetch(DEFAULT_CODE_URL)
-    .then((response) => response.text())
-    .catch(() => undefined);
+  const [tabValue, setTabValue] = useState('Experience');
+  const handleTabChange = useCallback(
+    (_event: SyntheticEvent, newValue: string) => setTabValue(newValue),
+    [],
+  );
 
-  return { props: { defaultCode } };
-}
-
-export default function Home({ defaultCode }: { defaultCode: string }) {
-  const monaco = useMonaco();
-
-  useEffect(() => {
-    if (monaco) {
-      monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-        jsx: monaco.languages.typescript.JsxEmit.Preserve,
-      });
-    }
-  }, [monaco]);
+  const tabs = useMemo(
+    () => [
+      {
+        content: Experience,
+        flatOrder: 1,
+        label: 'Experience',
+      },
+      {
+        content: Education,
+        flatOrder: 2,
+        label: 'Education',
+      },
+      {
+        content: Tech,
+        flatOrder: 0,
+        label: 'Tech',
+      },
+    ],
+    [],
+  );
+  const flatTabs = useMemo(
+    () =>
+      [...tabs]
+        .sort((tabA, tabB) => tabA.flatOrder - tabB.flatOrder)
+        .map((tab) => (
+          <Stack key={tab.label} spacing={2}>
+            <Typography variant="h4">{tab.label}</Typography>
+            <tab.content />
+          </Stack>
+        )),
+    [tabs],
+  );
 
   return (
     <>
       <Head>
         <title>Dodgez Dev</title>
       </Head>
-      <Box flexGrow={1} height="100%">
-        <Editor
-          defaultPath={DEFAULT_CODE_URL.split('/').pop()}
-          defaultValue={defaultCode}
-          theme="vs-dark"
-        />
+      <Box p={8}>
+        <Stack spacing={8}>
+          <ProfileBlurb />
+          <AboutBlurb />
+          <Stack spacing={1}>
+            <Box display="inline">
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={isTabbedView}
+                    onChange={handleViewChange}
+                    name="tab-view"
+                  />
+                }
+                label="Tabbed view"
+              />
+            </Box>
+            {!isTabbedView ? (
+              <Stack pt={6} spacing={8}>
+                {flatTabs}
+              </Stack>
+            ) : (
+              <TabContext value={tabValue}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                  <TabList
+                    aria-label="About sections"
+                    onChange={handleTabChange}
+                  >
+                    {tabs.map((tab) => (
+                      <Tab
+                        key={tab.label}
+                        label={tab.label}
+                        value={tab.label}
+                      />
+                    ))}
+                  </TabList>
+                </Box>
+                {tabs.map((tab) => (
+                  <TabPanel
+                    key={tab.label}
+                    sx={{ marginTop: '0px !important' }}
+                    value={tab.label}
+                  >
+                    <tab.content />
+                  </TabPanel>
+                ))}
+              </TabContext>
+            )}
+          </Stack>
+        </Stack>
       </Box>
     </>
   );
