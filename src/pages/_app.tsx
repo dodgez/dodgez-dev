06219c407163b003250b3fc2f4d73700257333ef
dodgez <zacharysdodge@gmail.com>
2023-08-10 21:02:24 -0700

    fix: color scheme on scrollbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,7 +31,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <CssBaseline />
+      <CssBaseline enableColorScheme />
       <Box
         display="flex"
         flexDirection="column"
