08da6b5810373f48aa7fec1ea365a2688018c336
Zachary Dodge <zacharysdodge@gmail.com>
2024-11-01 20:09:45 -0700

    build: meta env types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /* eslint-disable */
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_API_KEY: string;
+  readonly PUBLIC_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
