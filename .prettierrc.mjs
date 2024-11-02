2fc46a3ef5879b0b16fafedf83d7642d5f18c61b
Zachary Dodge <zacharysdodge@gmail.com>
2024-11-01 19:32:11 -0700

    fix: merge changes and formatting

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+export default {
+    overrides: [
+        {
+            files: '*.astro',
+            options: {
+                parser: 'astro',
+            },
+        },
+    ],
+    plugins: ['prettier-plugin-astro'],
+    singleQuote: true,
+    trailingComma: "all",
+};
