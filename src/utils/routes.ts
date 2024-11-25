dbac4d9b0a5246f3b7651f037314f52d782786c8
dodgez <zacharysdodge@gmail.com>
2024-11-25 15:45:13 -0800

    feat: initial astro content

---
+++ src/utils/routes.ts
@@ -0,0 +1,14 @@
+const routes = {
+    'about': {
+        path: '/',
+        text: "About me",
+        title: "About Zachary Dodge",
+    },
+    'wordle-filter': {
+        path: '/wordle-filter',
+        text: "Wordle Filter",
+        title: "Zach's Wordle Filter",
+    },
+};
+
+export default routes;
